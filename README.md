43bde50df9340d91662135893d47972b96b5842f
Dan Finlay <dan@danfinlay.com>
2013-05-16 23:34:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Dan's first [Baudio](https://github.com/substack/baudio) experiments.
+Inspired by Substack's talk today at Node PDX.  I'm grateful it was being live-streamed!
---
+++ app.js
@@ -0,0 +1,49 @@
+var baudio = require('baudio');
+
+var root = 1000;
+var beat = 9;
+var changeFrequency = 5000;
+
+var components = [
+	function(t){
+		return Math.sin(t*root) + Math.sin(t*(root+beat));
+	},
+	function(t){
+		return Math.sin(t*root * 3);
+	},
+	function(t){
+		if(t % (root*beat) < (root*beat/2)){
+			return 1;
+		}else{
+			return 0;
+		}
+	},
+	function(t){
+		if(t/20>10){
+			return Math.cos(t*500)
+		}
+		return 0;
+	},
+	function(t){
+		return Math.sin(t*root/10);
+	},
+	function(t){
+		return Math.sin(t*root/20);
+	}
+]
+
+function componentSum(t){
+
+	var result = 0;
+	components.forEach(function(component){
+		result+=component(t);
+	})
+    return result;
+}
+	
+var b = baudio(function (t) {
+    return componentSum(t);
+});
+
+b.play();
+b.record('./result')
